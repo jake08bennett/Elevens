967ce8279745540f4b371a2e9adeb0c11776cb2b
jake08bennett <jake08bennett@hotmail.com>
2016-04-12 09:42:22 -0600

    a1 done

--- src/Card.java
+++ src/Card.java
@@ -8,31 +8,34 @@ public class Card {
 	/**
 	 * String value that holds the suit of the card
 	 */
-	private String suit;
+	private String cardSuit;
 
 	/**
-	 * String value that holds the rank of the card
+	 * String value that holds the cardRank of the card
 	 */
-	private String rank;
+	private String cardRank;
 
 	/**
 	 * int value that holds the point value.
 	 */
-	private int pointValue;
+	private int cardPointValue;
 
 
    /**
 	 * Creates a new <code>Card</code> instance.
 	 *
-	 * @param cardRank  a <code>String</code> value
-	 *                  containing the rank of the card
+	 * @param cardcardRank  a <code>String</code> value
+	 *                  containing the cardRank of the card
 	 * @param cardSuit  a <code>String</code> value
 	 *                  containing the suit of the card
-	 * @param cardPointValue an <code>int</code> value
+	 * @param cardcardPointValue an <code>int</code> value
 	 *                  containing the point value of the card
 	 */
-	public Card(String cardRank, String cardSuit, int cardPointValue) {
+	public Card(String cardcardRank, String cardSuit, int cardcardPointValue) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		 this.cardSuit = cardSuit;
+	     this.cardRank = cardcardRank;
+	     this.cardPointValue = cardcardPointValue;
 	}
 
 
@@ -42,46 +45,52 @@ public class Card {
 	 */
 	public String suit() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return this.cardSuit;
    }
 
 	/**
-	 * Accesses this <code>Card's</code> rank.
-	 * @return this <code>Card's</code> rank.
+	 * Accesses this <code>Card's</code> cardRank.
+	 * @return this <code>Card's</code> cardRank.
 	 */
-	public String rank() {
+	public String cardRank() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return this.cardRank;
 	}
 
    /**
 	 * Accesses this <code>Card's</code> point value.
 	 * @return this <code>Card's</code> point value.
 	 */
-	public int pointValue() {
+	public int cardPointValue() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return this.cardPointValue;
 	}
 
 	/** Compare this card with the argument.
 	 * @param otherCard the other card to compare to this
-	 * @return true if the rank, suit, and point value of this card
+	 * @return true if the cardRank, suit, and point value of this card
 	 *              are equal to those of the argument;
 	 *         false otherwise.
 	 */
 	public boolean matches(Card otherCard) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return this.cardSuit.equals(otherCard.cardSuit) && this.cardRank.equals(otherCard.cardRank) && this.cardPointValue == otherCard.cardPointValue;
+	       
 	}
 
 	/**
-	 * Converts the rank, suit, and point value into a string in the format
-	 *     "[Rank] of [Suit] (point value = [PointValue])".
+	 * Converts the cardRank, suit, and point value into a string in the format
+	 *     "[cardRank] of [Suit] (point value = [cardPointValue])".
 	 * This provides a useful way of printing the contents
 	 * of a <code>Deck</code> in an easily readable format or performing
 	 * other similar functions.
 	 *
-	 * @return a <code>String</code> containing the rank, suit,
+	 * @return a <code>String</code> containing the cardRank, suit,
 	 *         and point value of the card.
 	 */
 	@Override
 	public String toString() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return "The card of " + this.cardRank + " has a cardRank of " + this.cardSuit + ". The point value is  " + this.cardPointValue;
 	}
 }
--- src/CardTester.java
+++ src/CardTester.java
@@ -9,5 +9,16 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("Spades", "8", 8);
+		Card card2 = new Card("Spades", "8", 8);
+		Card card3 = new Card("Clubs", "King", 13);
+		
+		System.out.println(card1.toString());
+		System.out.println(card2.toString());
+		System.out.println(card3.toString());
+		
+		System.out.println("do cards 3 and 2 match? " + card2.matches(card3));
+		System.out.println("do cards 1 and 3 match?  " + card1.matches(card3));
+		System.out.println("do cards 1 and 2 match? " + card1.matches(card2));
 	}
 }
